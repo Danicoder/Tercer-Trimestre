56834512332657af42876871a34edf082f1ed7f1
Danicoder <56139050+Danicoder@users.noreply.github.com>
2021-04-12 18:39:08 +0200

    Ejercicios oficiales
    Listas

---
+++ EjerciciosOficialesListas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EjerciciosOficialesListas
+{
+    class Program
+    {
+        static void Main()
+        {
+            tpersona Person1 = new tpersona();
+            Person1.SetEdad(25);
+            Person1.Setnombre("Manuel");
+            Person1.Settelefono("6428567892");
+            Person1.Setsexo("Masculino");
+            Person1.Setcasado(true);
+
+
+            Console.WriteLine("La edad es:{0} ",Person1.Getedad());
+            Console.WriteLine("El nombre es:{0} ", Person1.Getnombre());
+            Console.WriteLine("El teléfono es:{0} ", Person1.Gettelefono());
+            Console.WriteLine("El Sexo es:{0} ", Person1.Getsexo());
+            if(Person1.Getcasado() == true)
+            {
+                Console.WriteLine("Es casado ");
+            }
+            else Console.WriteLine("No es casado ");
+        }
+    }
+}
---
+++ EjerciciosOficialesListas/tpersona.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjerciciosOficialesListas
+{
+    class tpersona
+    {
+        /*Crear la clase (Orientado a Objetos) necesaria para almacenar los datos de una persona.
+        Los datos que se quieren almacenar serán su nombre, edad, teléfono, sexo y si está
+        casado. Tendremos un botón desde el que leeremos los datos y otro botón desde el que
+        los mostraremos. (Ejercicio Resuelto).*/
+
+        string nombre;
+        int edad;
+        string telefono;
+        string sexo;
+        bool casado;
+
+        public void SetEdad(int edad)
+        {
+            this.edad = edad;
+        }
+        public int Getedad()
+        {
+            return this.edad;
+        }
+        public void Setnombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public string Getnombre()
+        {
+            return this.nombre;
+        }
+        public void Settelefono(string telefono)
+        {
+            this.telefono = telefono;
+        }
+        public string Gettelefono()
+        {
+            return telefono;
+        }
+        public void Setsexo(string sexo)
+        {
+            this.sexo = sexo;
+        }
+        public string Getsexo()
+        {
+            return this.sexo;
+        }
+        public void Setcasado(bool casado)
+        {
+            this.casado = casado;
+        }
+        public bool Getcasado()
+        {
+            return this.casado;
+        }
+        /*Apoyándoos en el ejercicio anterior, realizar un programa que trabaje con una lista de
+        personas. Realizar un botón para leer los datos de una persona y otro para imprimir los
+        datos de todas las personas.*/
+    }
+}
